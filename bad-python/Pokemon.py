05ceee47fa38224f727a2de122aa00fd0ec507cd
23092 <23092@wc.school.nz>
2026-03-02 15:00:24 +1300

    rah

--- bad-python/Pokemon.py
+++ bad-python/Pokemon.py
@@ -6,21 +6,39 @@ c=17
 d=35
 
 
-natrix = [{"yyy":"color"},{"ert"}]
-print (natrix)
 
 wild_pokemon = [
-    {"Name":"Charizard","Type":"Fire","Level":random.randint(a,b),"Health":random.randint(c,d)},
-    {"Name":"Venasaur","Type":"Grass","Level":random.randint(a,b),"Health":random.randint(c,d)},
-    {"Name":"Blastoise","Type":"Grass","Level":random.randint(a,b),"Health":random.randint(c,d)},
-    {"Name":"Gardivour","Type":"Fairy","Level":random.randint(a,b),"Health":random.randint(c,d)},
-    {"Name":"Vaporeon","Type":"Water","Level":random.randint(a,b),"Health":random.randint(c,d)},
-    {"Name":"Leafeon","Type":"Grass","Level":random.randint(a,b),"Health":random.randint(c,d)}
+    {"Name":"Charizard",
+     "Type":"Fire",
+     "Level":random.randint(a,b),
+     "Health":random.randint(c,d)},
+    {"Name":"Venasaur",
+     "Type":"Grass",
+     "Level":random.randint(a,b),
+     "Health":random.randint(c,d)},
+    {"Name":"Blastoise",
+     "Type":"Grass",
+     "Level":random.randint(a,b),
+     "Health":random.randint(c,d)},
+    {"Name":"Gardivour",
+     "Type":"Fairy",
+     "Level":random.randint(a,b),
+     "Health":random.randint(c,d)},
+    {"Name":"Vaporeon",
+     "Type":"Water",
+     "Level":random.randint(a,b),
+     "Health":random.randint(c,d)},
+    {"Name":"Leafeon",
+     "Type":"Grass",
+     "Level":random.randint(a,b),
+     "Health":random.randint(c,d)}
 
 ]
 
-own_pokemon =[{"Name":"Pidgy","Type":"Flying","Level":3,"Health":20,"Attack":["Flap":,random.randint(a,b)]}]
-print(wild_pokemon)
+
+own_pokemon =[{"Name":"Pidgy","Type":"Flying","Level":3,"Health":20,"c_Health":int(20),"Attack":["Flap",(random.randint(a,b))]}]
+#print(wild_pokemon[0]['Level'])
+#time.sleep(10)
 
 
 def overworld_timer():
@@ -31,7 +49,10 @@ def overworld_timer():
     battle()
 
 def enemy_attack(pokemon):
-
+    print("The pokemon is called:", wild_pokemon[pokemon]['Name'])
+    print("The pokemon level is:",wild_pokemon[pokemon]['Level'])
+    own_pokemon[0]["c_Health"] - (wild_pokemon[pokemon["Level"]])
+    print("Health is now: ", (own_pokemon[0]['c_Health']))
 
 def battle():
     x=random.randint(0,len(wild_pokemon)-1)
