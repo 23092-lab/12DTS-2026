1c5636963e5f50fd4eff62abe33bb5127288f68f
23092 <23092@wc.school.nz>
2026-03-04 09:23:47 +1300

    big c

--- pokemon-game/mon.py
+++ pokemon-game/mon.py
@@ -18,13 +18,21 @@ def let_there_be_life():
         print(wild_pokemon[x])
 
 def pick_pokemon():
-    print("Type pokemon name: ")
-    name = input()
-    print(get.wild_pokemon(name,"Not Found"))
-    own_pokemon.append(wild_pokemon[1][name])
-    print(own_pokemon)
+    loop=1
+    while loop==1:
+        print("Type pokemon name: ")
+        name = input().strip()
+        match = next((i for i in wild_pokemon if i['NAME'].lower() == name.lower()), None)
+        if match:
+            own_pokemon.append(match.copy())
+            print(f"You have found: {match['NAME']}.")
+            loop =0
+        else:
+            print(f"Error: '{name}' was not found in the wild.")
 def battle_loop():
     pass
+
 let_there_be_life()
 print(wild_pokemon)
 pick_pokemon()
+print(own_pokemon)
