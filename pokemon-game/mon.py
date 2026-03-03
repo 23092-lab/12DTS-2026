955d39e01c48835504a686e77e89649413ae89e8
23092 <23092@wc.school.nz>
2026-03-03 13:06:00 +1300

    during cshool

--- pokemon-game/mon.py
+++ pokemon-game/mon.py
@@ -5,12 +5,7 @@ import names as n
 
 wild_pokemon=[]
 own_pokemon=[]
-#for x in m.POKEMON_NAMES: #increment for amount of pokemon
-#    wild_pokemon.append({"NAME": x,
-#                            "Level": random.randint(m.MIN_LEVEL, m.MAX_LEVEL),
-#                            "Health": random.randint(m.MIN_HEALTH, m.MAX_HEALTH)
-#                            }
-#   )
+
 def let_there_be_life():
     wild_pokemon.clear()
     for x in range(0,len(n.all_pokemon)):
@@ -23,7 +18,7 @@ def let_there_be_life():
         print(wild_pokemon[x])
 
 def pick_pokemon():
-    print("Type name: ")
+    print("Type pokemon name: ")
     name = input()
     own_pokemon.append(wild_pokemon[1][name])
     print(own_pokemon)
