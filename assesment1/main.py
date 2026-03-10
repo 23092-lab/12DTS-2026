6d28cb0dbc03f03f7882508503a77d6a20369cc0
23092 <23092@wc.school.nz>
2026-03-11 09:37:34 +1300

    new isde

--- assesment1/main.py
+++ assesment1/main.py
@@ -8,6 +8,8 @@
 import random
 import time
 # Variables
+NO_PUZZLES=3
+completion=[]
 inventory=[]
 loop=1
 choice=0
@@ -19,13 +21,11 @@ def bush_city():
         if int(input("solve the puzzle "))==1:
             loop = 0
             inventory.append("done")
+            completion[0]=True
             print (inventory)
             game_check()
             print("e")
             break
-    choice()
-
-
 
 def waitangi():
     print("================================================\nWelcome to Level 4, Treaty of Waitangi")
@@ -45,6 +45,7 @@ def range_check(x,min,max):
 def game_check():
     if len(inventory)==3: # check for appends to list as amount of pieces
         print("thank you for playing the game, the program will now self destruct in 5")
+        time.sleep(0.5)
         print("4")
         print("3")
         print("2")
@@ -57,7 +58,23 @@ def choice():
     loop=1
     while loop==1:
         try:
-            choice = int(input("================================================\nWhere do you want to go?\n1. Bush City\n2. Level 4, Treaty of Waitangi: Signs of a Nation\n3. Level 5, Art Studio \n4. Cafe (HINT)\n:"))
+            print("================================================\nWhere do you want to go?")
+            print(completion)
+            if completion[0]==False:
+                print("1. Bush City")
+            if completion[0]==True:
+                print("1. (COMPLETED) Bush City")
+            if completion[1]==False:
+                print("2. Treaty of Waitangi")
+            if completion[1]==True:
+                print("2. (COMPLETED) Treaty of Waitangi")
+            if completion[2]==False:
+                print("3. Art Studio")
+            if completion[2]==True:
+                print("3. (COMPLETED) Art Studio")
+            choice = int(input())
+            #choice = int(input("================================================\nWhere do you want to go?\n1. Bush City\n2. Level 4, Treaty of Waitangi: Signs of a Nation\n3. Level 5, Art Studio \n4. Cafe (HINT)\n:"))
+
             # fix choice to individual check so know when one place is done
             #start range check if valid between choices
             if range_check(choice,1,4)==1:
@@ -76,10 +93,15 @@ def choice():
         art_studio()
     if choice==4:
         cafe()
-
+def main():
+    while 1:
+        starting_sequence()
 def starting_sequence():
     print("================================================\nWelcome to Te Papa!\nWe are in desperate need of someone to find the scattered pieces of the Treaty of Waitangi!"
           "\nTo find the pieces, go to each floor and solve the puzzles on that floor")
     choice()
 # Main
-starting_sequence()
+for i in range(0,NO_PUZZLES):
+    completion.append(False)
+main()
+
---
+++ assesment1/second.py
@@ -0,0 +1,7 @@
+import lib as l
+
+
+def opening():
+    print("kiwi fights")
+    print("you must journey, across the land")
+
