0d312889592c480c2a798ac467db6b26c89b9462
23092 <23092@wc.school.nz>
2026-04-01 08:59:10 +1300

    push

--- assesment1/assessment.py
+++ assesment1/assessment.py
@@ -5,18 +5,15 @@ import copy as cp
 import time as t
 
 # variables
-
-
 ENEMIES = {{"NAME": "Tui", "HP": r.randint(15, 20), "SPD": r.randint(40, 50), "ATK": r.randint(6, 10), },
            {"NAME": "Kereru", "HP": r.randint(20, 30), "SPD": r.randint(35, 45), "ATK": r.randint(5, 15), },
            {"NAME": "Weka", "HP": r.randint(25, 35), "SPD": r.randint(30, 41), "ATK": r.randint(7, 14), },
            {"NAME": "Kakapo", "HP": r.randint(30, 35), "SPD": r.randint(25, 35), "ATK": r.randint(10, 14), },
            {"NAME": "Pukeko", "HP": r.randint(40, 55), "SPD": r.randint(15, 25), "ATK": r.randint(20, 40), }, }
-
 # change all hp stuff here
-HP = 20
+HP = 50
 SPD = 30
-ATK = 25
+ATK = 35
 
 enemy = []
 treaty_parts = 0
