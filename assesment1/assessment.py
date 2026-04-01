382e6f9e1f82e16b4b7e6e625295af418799881f
23092 <23092@wc.school.nz>
2026-04-02 09:15:04 +1300

    yes

--- assesment1/assessment.py
+++ assesment1/assessment.py
@@ -5,12 +5,16 @@ import copy as cp
 import time as t
 
 # variables
-ENEMIES = {{"NAME": "Tui", "HP": r.randint(15, 20), "SPD": r.randint(40, 50), "ATK": r.randint(6, 10), },
-           {"NAME": "Kereru", "HP": r.randint(20, 30), "SPD": r.randint(35, 45), "ATK": r.randint(5, 15), },
-           {"NAME": "Weka", "HP": r.randint(25, 35), "SPD": r.randint(30, 41), "ATK": r.randint(7, 14), },
-           {"NAME": "Kakapo", "HP": r.randint(30, 35), "SPD": r.randint(25, 35), "ATK": r.randint(10, 14), },
-           {"NAME": "Pukeko", "HP": r.randint(40, 55), "SPD": r.randint(15, 25), "ATK": r.randint(20, 40), }, }
+ENEMIES = ({"NAME": "Tui", "HP": r.randint(e_HP, 20), "SPD": r.randint(e_SPD, 50), "ATK": r.randint(6, 10), },
+           {"NAME": "Kereru", "HP": r.randint(e_HP, 30), "SPD": r.randint(e_SPD, 45), "ATK": r.randint(5, 15), },
+           {"NAME": "Weka", "HP": r.randint(e_HP, 35), "SPD": r.randint(e_SPD, 41), "ATK": r.randint(7, 14), },
+           {"NAME": "Kakapo", "HP": r.randint(e_HP, 35), "SPD": r.randint(e_SPD, 35), "ATK": r.randint(10, 14), },
+           {"NAME": "Pukeko", "HP": r.randint(e_HP, 55), "SPD": r.randint(e_SPD, 25), "ATK": r.randint(20, 40), }, )
 # change all hp stuff here
+e_HP=15
+e_SPD=15
+e_ATK=15
+
 HP = 50
 SPD = 30
 ATK = 35
