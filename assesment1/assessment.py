096f431c415d3ff6e4c5a00113efc01f8c50e49c
coriander67 <coriander6781@gmail.com>
2026-04-02 22:35:44 +1300

    final one

--- assesment1/assessment.py
+++ assesment1/assessment.py
@@ -7,33 +7,38 @@ import time as t
 # variables
 
 # change all hp stuff here
+#initialize enemy stats first and then randomizes for later
 e_HP=20
 e_SPD=15
 e_ATK=10
-
+# randomize here
 ENEMIES = ({"NAME": "Tui", "HP": r.randint(e_HP, e_HP+20), "SPD": r.randint(e_SPD, e_SPD+20), "ATK": r.randint(e_ATK, e_ATK+5), },
            {"NAME": "Kereru", "HP": r.randint(e_HP, e_HP+10), "SPD": r.randint(e_SPD, e_SPD+10), "ATK": r.randint(e_ATK, e_ATK+5), },
            {"NAME": "Weka", "HP": r.randint(e_HP, e_HP+10), "SPD": r.randint(e_SPD, e_SPD+5), "ATK": r.randint(e_ATK, e_ATK+5), },
            {"NAME": "Kakapo", "HP": r.randint(e_HP, e_HP+10), "SPD": r.randint(e_SPD, e_SPD+5), "ATK": r.randint(e_ATK, e_ATK+10), },
            {"NAME": "Pukeko", "HP": r.randint(e_HP, e_HP+10), "SPD": r.randint(e_SPD, e_SPD+5), "ATK": r.randint(e_ATK, e_ATK+10), }, )
 
-HP = 50
+#initialize player stats
+HP = 40
 SPD = 30
 ATK = 35
 
-enemy = []
-treaty_parts = 0
+enemy = []  # used as a writable list that gets copies from the constant list ENEMIES
 player = [{"NAME": "", "B_HP": HP, "B_SPD": SPD, "B_ATK": ATK}]
+
 healing_factor = 0.4
 boost = 1.2
+
+treaty_parts = 0
+
 
 
 # function methods
 def valuecheck(low, hi):  # Start of the function that makes sure user inputs a valid number
     while 1:  # WILL RUN FOREVER UNTIL HAS RETURNED A VALUE
-        try:
+        try:  # for value error detection
             loop = 1
-            while loop == 1:
+            while loop == 1: # for if input is within given range detection
                 choose = int(input(":"))
                 if choose < low:
                     print("Try again")
@@ -51,23 +56,22 @@ def damage(n):  # Changes enemy or player health depending on given n value
         player[0]["B_HP"] -= r.randint(enemy[0]["ATK"] - 5, enemy[0]["ATK"])
         print("Your HP now: ", player[0]["B_HP"])
     if n == 0:
-        print(player[0]["B_ATK"])
+        #print(player[0]["B_ATK"]) not needed anymore
         enemy[0]["HP"] -= r.randint(player[0]["B_ATK"] - 5, player[0]["B_ATK"])
         print("Enemy HP: ", enemy[0]["HP"])
 
 
-def dead(thing):  # Quick check to make sure whatever as thing is dead or not
+def dead(thing):  # Quick check to make sure whatever thing is dead or not
     if thing <= 0:
         return 1
 
 
 def opening():
     global player  # To allow writing into player list for name and stat boosts
-    player = [{"NAME": "", "B_HP": HP, "B_SPD": SPD, "B_ATK": ATK}]
-    print("==============================\nWelcome to New Zealand you have to find the Treaty of Waitangi\n"
-          "that 6 "
-          "birds have ate for breakfast!")
-    player[0]["NAME"] = input("Enter your name: ")  # Decided not to use any checks whether name is valid or not
+    player = [{"NAME": "", "B_HP": HP, "B_SPD": SPD, "B_ATK": ATK}]  # reinitializes for each reset of the game
+    print("\n==============================\nWelcome to New Zealand,\nYour task is to piece together the Treaty of Waitangi\n"
+          "that 6 birds have digested as a meal!")
+    player[0]["NAME"] = input("\nEnter your name: ")  # Decided not to use any checks whether name is valid or not
     # because user might input a name with numbers and characters
     print("\nWelcome, ", player[0]["NAME"], "!")
     print("\nThis game only requires you to input the number keys!\nSo typing out the words will not work!!")
@@ -84,13 +88,13 @@ def opening():
 
 def fight():
     global enemy
-    global treaty_parts
+    global treaty_parts  # needs to be global to be able to write to value in order to not be a local variable within the function
     print("==============================\nYOU ARE FIGHTING...")
     if len(enemy) == 0:  # Checks if enemy exists
-        enemy.append(cp.deepcopy(ENEMIES[r.randint(0, len(ENEMIES) - 1)]))
-    elif enemy[0]["HP"] <= 0:  # Checks if enemy HP below 0 (probably delete later now copy working)
-        enemy.clear()
-        enemy.append(cp.deepcopy(ENEMIES[r.randint(0, len(ENEMIES) - 1)]))
+        enemy.append(cp.deepcopy(ENEMIES[r.randint(0, len(ENEMIES) - 1)]))  # if it doesn't exist copies random from ENEMY 
+    #elif enemy[0]["HP"] <= 0:  # Checks if enemy HP below 0 (probably delete later now copy working)
+    #    enemy.clear()
+    #    enemy.append(cp.deepcopy(ENEMIES[r.randint(0, len(ENEMIES) - 1)]))
     print("THE", enemy[0]["NAME"], "!!", "\n","HP: ", enemy[0]["HP"],)# "\n", "ATK range: ", enemy[0]["ATK"] - 5, "-",
           #enemy[0]["ATK"])  # Description of enemy
 
@@ -121,7 +125,7 @@ def fight():
                 enemy.clear()  # Clear enemy ready for next loop
                 treaty_parts += 1  # Add one treaty part
                 print("OBTAINED: Part", treaty_parts, "of treaty of Waitangi")
-                break
+                break  # breaks out to inter so it can detect whether to put player back into battle or end game
             else:
                 pass  # should loop back to start until either player or enemy hp is =< 0
         if choice == 2:  # HEAL CHOICE
@@ -139,9 +143,9 @@ def fight():
 def inter():
     global treaty_parts
     while 1:
-        if dead(player[0]["B_HP"]) == 1:  # Player is dead comes back to here
+        if dead(player[0]["B_HP"]) == 1:  # when Player is dead comes to here
             print("==============================\nYou have lost!\nBetter luck next time!")
-            t.sleep(0.5)
+            t.sleep(1.5)
             treaty_parts = 0
             break
         if treaty_parts < 6:  # Game is still running
